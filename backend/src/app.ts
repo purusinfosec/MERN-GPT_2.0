fd40eb1dfb7f79c598958b1f188e8185d33f1507
Purusottam Paswan <purusottampaswan998@gmail.com>
2025-10-18 05:03:57 +0530

    v1.4

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,7 +12,10 @@ const app = express();
 
 
 //middlewares
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://mern-gpt-2-0-frontend.vercel.app", credentials: true }));
+
+
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
